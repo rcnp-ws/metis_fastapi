df6f09bde487850bab22503a077efaee576967d7
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2024-07-13 23:11:58 +0900

    exe --> exec, ret --> {message: ret}

--- routers/RouterSystemCommand.py
+++ routers/RouterSystemCommand.py
@@ -10,10 +10,10 @@ router = APIRouter(
 async def root() : 
    pass
 
-@router.get('/exe/{cmd}')
-async def exe(cmd: str):
+@router.get('/exec/{cmd}')
+async def exec(cmd: str):
    try:
       ret = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
    except:
       ret = "Error in subprocess.check_call(" + cmd + ")"
-   return ret
+   return {"message": ret}
